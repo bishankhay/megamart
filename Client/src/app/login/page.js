52ceceb90fdcc3d825cb12822424988ff2b93df5
bishankhay <bishankhay@gmail.com>
2024-06-26 07:12:16 +0545

    modification

--- Client/src/app/login/page.js
+++ Client/src/app/login/page.js
@@ -1,64 +1,33 @@
 'use client'
-import React from 'react';
-import { Formik } from 'formik';
-import {render} from 'react-dom'
-const login = () => (
-  <div>
-    <h1>LOGIN</h1>
-    <Formik
-      initialValues={{ email: '', password: '' }}
-      validate={values => {
-        const errors = {};
-        if (!values.email) {
-          errors.email = 'Required';
-        } else if (
-          !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-        ) {
-          errors.email = 'Invalid email address';
-        }
-        return errors;
-      }}
-      onSubmit={(values, { setSubmitting }) => {
-        setTimeout(() => {
-          alert(JSON.stringify(values, null, 2));
-          setSubmitting(false);
-        }, 400);
-      }}
-    >
-      {({
-        values,
-        errors,
-        touched,
-        handleChange,
-        handleBlur,
-        handleSubmit,
-        isSubmitting,
-        /* and other goodies */
-      }) => (
-        <form onSubmit={handleSubmit}>
-          <input
-            type="email"
-            name="email"
-            onChange={handleChange}
-            onBlur={handleBlur}
-            value={values.email}
-          />
-          {errors.email && touched.email && errors.email}
-          <input
-            type="password"
-            name="password"
-            onChange={handleChange}
-            onBlur={handleBlur}
-            value={values.password}
-          />
-          {errors.password && touched.password && errors.password}
-          <button type="submit" disabled={isSubmitting}>
-            Submit
-          </button>
-        </form>
-      )}
-    </Formik>
-  </div>
-);
+// import React from 'react'
 
-export default login;
+// const login = () => {
+//   return (
+//     <div className='warpper'>
+//       <form action="">
+//         <h1>Login</h1>
+//         <div className="input-box">
+//           <input type="text" placeholder='username'/>
+//         </div>
+//         <div className="input-box">
+//           <input type="password" placeholder='password'/>
+//         </div>
+        
+//       </form>
+//     </div>
+//   )
+// }
+
+// export default login
+
+import React from 'react'
+
+const login = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default login
